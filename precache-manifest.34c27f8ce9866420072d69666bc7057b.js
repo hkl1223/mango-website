a43c57b81310278e40e1f17c6c8a14e43ca71d0b
hkl1223 <8465044+hkl1223@user.noreply.gitee.com>
2021-01-06 14:33:21 +0800

    first commit

---
+++ precache-manifest.34c27f8ce9866420072d69666bc7057b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6a2abcf02bd8d8fddb",
+    "url": "/mango-website/css/app.002206aa.css"
+  },
+  {
+    "revision": "5853b2e6aa6e09a0959b136199e2953e",
+    "url": "/mango-website/index.html"
+  },
+  {
+    "revision": "7e6a2abcf02bd8d8fddb",
+    "url": "/mango-website/js/app.5700fc0c.js"
+  },
+  {
+    "revision": "655ebfb586d7c687d3d6",
+    "url": "/mango-website/js/chunk-vendors.3f109135.js"
+  },
+  {
+    "revision": "ee544f796d0056606c7a6c4737282730",
+    "url": "/mango-website/mango.png"
+  },
+  {
+    "revision": "28a03ee60c6dc36a370ef5154535c651",
+    "url": "/mango-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mango-website/robots.txt"
+  }
+]);
